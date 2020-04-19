fb0a3da2238f3a4514af4427bec29b7b3501fddc
Andrey Doroshenko <dronskmnablya@mail.ru>
2020-04-19 16:06:23 +0300

    Исправления js и расположения изображений

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 
-let popup = document.querySelector('.popup'), 
+const popup = document.querySelector('.popup'), 
 popupToggle = document.querySelector('.profile__edit-button'), 
 popupClose = document.querySelector('.popup__closed'), 
 popupName = document.querySelector('.popup__input_name'), 
 popupProfession = document.querySelector('.popup__input_profession'),
 popupSubmit = document.querySelector('.popup__submit-btn'); 
  
-let popupTitle = document.querySelector('.profile__title'),    
+const popupTitle = document.querySelector('.profile__title'),    
 popupSubtitle = document.querySelector('.profile__subtitle'); 
 
 
@@ -17,12 +17,12 @@ function openPopup() {
     popup.classList.add('popup_opened'); 
     popupName.value = popupTitle.textContent; 
     popupProfession.value = popupSubtitle.textContent; 
-}; 
-
+}; //открытие popup
+ 
 
 function closePopup() { 
     popup.classList.remove('popup_opened'); 
-}; 
+}; //закрытие popup
 
 
  
@@ -33,9 +33,11 @@ function formSubmitHandler (evt) {
 evt.preventDefault();  
 popupTitle.textContent = popupName.value; 
 popupSubtitle.textContent = popupProfession.value; 
-closePopup()
-} 
-popupSubmit.addEventListener('submit', formSubmitHandler); 
+closePopup();
+}; // отправка формы, измененние значений на странице
+
+
+
+popupForm.addEventListener('submit', formSubmitHandler); 
 popupClose.addEventListener('click', closePopup); 
-popupSubmit.addEventListener('click', formSubmitHandler); // Без этого слушателя перестают сохранятся значения введенные в инпут, в чем может быть ошибка?
 popupToggle.addEventListener('click', openPopup); 
